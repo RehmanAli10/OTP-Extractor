7e76587bfee34010e7312185b533ff09beead1e0
RehmanAli10 <vrehmanali@gmail.com>
2025-10-03 23:42:12 +0500

    updated

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController.js");
-const sharedKeyAuth=require("../middlewares/sharedKeyAuth.js");
+const sharedKeyAuth = require("../middlewares/sharedKeyAuth.js");
 
-// Define GET routes
-router.post("/register",sharedKeyAuth, authController.register);
-router.post("/login",sharedKeyAuth, authController.login);
-router.post("/verify-otp",sharedKeyAuth, authController.verifyTotp);
-router.get("/cache-diagnosis",sharedKeyAuth, authController.cacheDiagnostics   );
+router.post("/register", sharedKeyAuth, authController.register);
+router.post("/login", sharedKeyAuth, authController.login);
+router.post("/verify-otp", sharedKeyAuth, authController.verifyTotp);
+router.get("/cache-diagnosis", sharedKeyAuth, authController.cacheDiagnostics);
 
 module.exports = router;

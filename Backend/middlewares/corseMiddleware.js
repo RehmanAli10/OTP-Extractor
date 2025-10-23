4e5397c5caeff407868b46797430060b5fda04f2
RehmanAli10 <vrehmanali@gmail.com>
2025-10-23 18:39:38 +0500

    updated cors

--- Backend/middlewares/corseMiddleware.js
+++ Backend/middlewares/corseMiddleware.js
@@ -2,7 +2,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_URL,
+  origin: function (origin, callback) {
+    if (origin === process.env.CORS_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 });
 
 module.exports = corsMiddleware;
